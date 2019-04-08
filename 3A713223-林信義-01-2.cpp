0edf413c52aafe25453a14752e8c983f531919e1
thomasqq <48341359+thomasqq@users.noreply.github.com>
2019-04-08 23:37:22 +0800

    Add files via upload

---
+++ 3A713223-林信義-01-2.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void) {
+	
+	printf("hollow world 3A713223\n");
+	
+	system("PAUSE");
+	return 0;
+}
